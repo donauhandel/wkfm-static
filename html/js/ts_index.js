3152fa91ba53b22c951a8e3dc201123d8416a5d6
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-17 11:57:06 +0100

    should close #2

---
+++ html/js/ts_index.js
@@ -0,0 +1,132 @@
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: "QVddDUvAYGWHlHfdshFm5X1s9ceWDblJ",
+    nodes: [
+      {
+        host: "typesense.acdh-dev.oeaw.ac.at",
+        port: "443",
+        protocol: "https",
+      },
+    ],
+    cacheSearchResultsForSeconds: 2 * 60,
+  },
+  additionalSearchParameters: {
+    query_by: "full_text"
+  },
+});
+
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+  indexName: 'wkfm',
+  searchClient,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    autofocus: true,
+    cssClasses: {
+      form: 'form-inline',
+      input: 'form-control col-md-11',
+      submit: 'btn',
+      reset: 'btn'
+    },
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#hits',
+    cssClasses: {
+      item: "w-100"
+    },
+    templates: {
+      empty: "Keine Resultate für <q>{{ query }}</q>",
+      item(hit, { html, components }) {
+        return html`
+      <h4><a href='${hit.id}.html'>${hit.title}</a></h4>
+      <p>${hit._snippetResult.full_text.matchedWords.length > 0 ? components.Snippet({ hit, attribute: 'full_text' }) : ''}</p>
+      <p>${hit.persons.map((item) => html`<a href='${item.id}'><span class="badge rounded-pill m-1 bg-danger">${item}</span></a>`)}</p>`
+      }
+    },
+  }),
+
+  instantsearch.widgets.stats({
+    container: '#stats-container',
+    templates: {
+      text: `
+        {{#areHitsSorted}}
+          {{#hasNoSortedResults}}Keine Treffer{{/hasNoSortedResults}}
+          {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
+          {{#hasManySortedResults}}{{#helpers.formatNumber}}{{nbSortedHits}}{{/helpers.formatNumber}} Treffer {{/hasManySortedResults}}
+          aus {{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}
+        {{/areHitsSorted}}
+        {{^areHitsSorted}}
+          {{#hasNoResults}}Keine Treffer{{/hasNoResults}}
+          {{#hasOneResult}}1 Treffer{{/hasOneResult}}
+          {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} Treffer{{/hasManyResults}}
+        {{/areHitsSorted}}
+        gefunden in {{processingTimeMS}}ms
+      `,
+    }
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: '#refinement-list-persons',
+    attribute: 'persons',
+    searchable: true,
+    showMore: true,
+    searchablePlaceholder: 'Suche',
+    cssClasses: {
+      searchableInput: 'form-control form-control-sm mb-2 border-light-2',
+      searchableSubmit: 'd-none',
+      searchableReset: 'd-none',
+      showMore: 'btn btn-secondary btn-sm align-content-center',
+      list: 'list-unstyled',
+      count: 'badge m-2 badge-secondary',
+      label: 'd-flex align-items-center',
+      checkbox: 'm-2',
+    }
+  }),
+
+
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+    padding: 2,
+    cssClasses: {
+      list: 'pagination',
+      item: 'page-item',
+      link: 'page-link'
+    }
+  }),
+  instantsearch.widgets.clearRefinements({
+    container: '#clear-refinements',
+    templates: {
+      resetLabel: 'Filter zurücksetzen',
+    },
+    cssClasses: {
+      button: 'btn'
+    }
+  }),
+
+
+
+  instantsearch.widgets.currentRefinements({
+    container: '#current-refinements',
+    cssClasses: {
+      delete: 'btn',
+      label: 'badge'
+    }
+  })
+]);
+
+
+
+search.addWidgets([
+  instantsearch.widgets.configure({
+    attributesToSnippet: ['full_text'],
+  })
+]);
+
+
+
+search.start();
--- xslt/search.xsl
+++ xslt/search.xsl
@@ -1,33 +1,55 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet 
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-    xmlns:tei="http://www.tei-c.org/ns/1.0" xmlns:xs="http://www.w3.org/2001/XMLSchema"
-    version="2.0" exclude-result-prefixes="xsl tei xs">
+    xmlns:tei="http://www.tei-c.org/ns/1.0"
+    xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    xmlns:local="http://dse-static.foo.bar"
+    version="2.0" exclude-result-prefixes="xsl tei xs local">
     
     <xsl:output encoding="UTF-8" media-type="text/html" method="html" version="5.0" indent="yes" omit-xml-declaration="yes"/>
-    
     
     <xsl:import href="./partials/html_navbar.xsl"/>
     <xsl:import href="./partials/html_head.xsl"/>
     <xsl:import href="partials/html_footer.xsl"/>
+
+    <xsl:variable name="doc_title" select="'Volltextsuche'"/>
+
     <xsl:template match="/">
-        <xsl:variable name="doc_title" select="'Volltextsuche'"/>
-        <html  class="h-100">
+        <html class="h-100">
             <head>
                 <xsl:call-template name="html_head">
                     <xsl:with-param name="html_title" select="$doc_title"></xsl:with-param>
                 </xsl:call-template>
             </head>
-            
             <body class="d-flex flex-column h-100">
                 <xsl:call-template name="nav_bar"/>
+                <main class="flex-shrink-0">
                     <div class="container">
-                        <h1>
-                            <xsl:value-of select="$doc_title"/>
-                        </h1>
-                                            
+                        <h1 class="text-center"><xsl:value-of select="$doc_title"/></h1>
+                        <div class="ais-InstantSearch">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div id="stats-container"></div>
+                                    <div id="searchbox"></div>
+                                    <div id="current-refinements"></div>
+                                    <div id="clear-refinements"></div>
+                                    <h2 class="pt-2">Personen</h2>
+                                    <div id="refinement-list-persons"></div>
+                                </div>
+                                <div class="col-md-8">
+                                    <div id="hits"></div>
+                                    <div id="pagination"></div>
+                                </div>
+                            </div>
+                        </div>                          
                     </div>
-                    <xsl:call-template name="html_footer"/>
+                </main>
+                <xsl:call-template name="html_footer"/>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css" />
+                <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.46.0"></script>
+                <script
+                    src="https://cdn.jsdelivr.net/npm/typesense-instantsearch-adapter@2/dist/typesense-instantsearch-adapter.min.js"></script>
+                <script src="js/ts_index.js"></script>
             </body>
         </html>
     </xsl:template>
