8e31fd95bf2d7ce48eb9b50eb7c1d123af4224cc
csae8092 <p.andorfer@gmail.com>
2024-10-01 14:32:07 +0200

    wip facs rework arche-ingestion wf #17

--- arche/make_metadata.py
+++ arche/make_metadata.py
@@ -8,6 +8,10 @@ from acdh_tei_pyutils.utils import normalize_string, make_entity_label, nsmap, g
 from rdflib import Graph, Namespace, URIRef, RDF, Literal, XSD
 from tqdm import tqdm
 
+IMG_NAME = (
+    "Merkantil_und_Wechselgericht_Merkantilprotokoll_1_Reihe__Reihe_1_7_Protokoll_1_"
+)
+
 g = Graph().parse("arche/arche_constants.ttl")
 ACDH = Namespace("https://vocabs.acdh.oeaw.ac.at/schema#")
 G_REPO_OBJECTS = Graph().parse("arche/repo_objects_constants.ttl")
@@ -52,8 +56,10 @@ for x in tqdm(files, total=len(files)):
 print("processing data/editions")
 files = glob.glob("data/editions/*.xml")
 files = files[:20]
-for x in tqdm(files, total=len(files)):
+for i, x in enumerate(tqdm(sorted(files), total=len(files)), start=1):
     fname = os.path.split(x)[-1]
+    cur_numb = f"{i:04}"
+    print(cur_numb)
     shutil.copyfile(x, os.path.join(TO_INGEST, fname))
     doc = TeiReader(x)
     uri = URIRef(f"{ID}/{fname}")
@@ -84,6 +90,7 @@ for x in tqdm(files, total=len(files)):
         )
     g.add((uri, ACDH["hasTitle"], Literal(has_title, lang="de")))
 
+
 print("adding repo objects constants now")
 COLS = [ACDH["TopCollection"], ACDH["Collection"], ACDH["Resource"]]
 COL_URIS = set()
@@ -95,4 +102,22 @@ for x in COL_URIS:
         g.add((x, p, o))
 
 g.parse("arche/title_image.ttl")
+
+files = files[:20]
+for i, x in enumerate(tqdm(sorted(files), total=len(files)), start=1):
+    uri = URIRef(f"{ID}/{IMG_NAME}{cur_numb}.jpeg")
+    g.add((uri, RDF.type, ACDH["Resource"]))
+    title = f"""WSTLA, {uri.split("/")[-1]
+                        .replace("_", " ")
+                        .replace("Reihe  Reihe", "Reihe")
+                        .replace("Merkantilprotokoll 1 ", "Merkantilprotokoll 1, ")}"""
+    g.add((uri, ACDH["hasTitle"], Literal(title, lang="de")))
+    g.add((uri, ACDH["isPartOf"], URIRef(f"{ID}/facs")))
+    g.add((uri, ACDH["hasCategory"], URIRef("https://vocabs.acdh.oeaw.ac.at/archecategory/image")))
+    g.add((uri, ACDH["hasLicense"], URIRef("https://vocabs.acdh.oeaw.ac.at/archelicenses/cc-by-nd-4-0")))
+    g.add((uri, ACDH["hasLicensor"], URIRef("https://d-nb.info/gnd/2060831-7")))
+    g.add((uri, ACDH["hasOwner"], URIRef("https://d-nb.info/gnd/2060831-7")))
+    g.add((uri, ACDH["hasRightsHolder"], URIRef("https://d-nb.info/gnd/2060831-7")))
+    g.add((uri, ACDH["hasDepositor"], URIRef("https://d-nb.info/gnd/13140007X")))
+    g.add((uri, ACDH["hasMetadataCreator"], URIRef("https://d-nb.info/gnd/1043833846")))
 g.serialize(os.path.join(TO_INGEST, "arche.ttl"))
