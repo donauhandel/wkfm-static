06bccd7b51f68f32799e9f9d2b0f528789598fe5
csae8092 <p.andorfer@gmail.com>
2025-02-13 07:43:09 +0100

    feat: save mentiones of entities with missing ids into csv

--- find_non_existing_entities.py
+++ find_non_existing_entities.py
@@ -1,17 +1,10 @@
 import glob
+import os
+import pandas as pd
 
 from acdh_tei_pyutils.tei import TeiReader
-from acdh_tei_pyutils.utils import check_for_hash
+from acdh_tei_pyutils.utils import check_for_hash, normalize_string
 
-
-files = sorted(glob.glob("./data/editions/*xml"))
-
-
-refs = set()
-for x in files:
-    doc = TeiReader(x)
-    for item in doc.any_xpath(".//tei:rs[@ref]"):
-        refs.add(check_for_hash(item.attrib["ref"]))
 
 files = sorted(glob.glob("./data/indices/*xml"))
 
@@ -22,10 +15,26 @@ for x in files:
     for item in doc.any_xpath(".//tei:body//@xml:id"):
         ids.add(item)
 
-no_match = set()
-for x in refs:
-    if x not in ids:
-        no_match.add(x)
+files = sorted(glob.glob("./data/editions/*xml"))
 
-print(f"found following {len(no_match)} IDs without index-entries")
-print(no_match)
+
+refs = set()
+data = []
+for x in files:
+    f_name = os.path.split(x)[-1].replace(".xml", ".html")
+    url = f"https://wmp1.acdh.oeaw.ac.at/{f_name}"
+    doc = TeiReader(x)
+    for item in doc.any_xpath(".//tei:rs[@ref]"):
+        no_hash = check_for_hash(item.attrib["ref"])
+        if no_hash not in ids:
+            try:
+                text = normalize_string(item.text)
+            except AttributeError:
+                text = "no text in rs"
+            data.append([
+                no_hash, text, url
+            ])
+            refs.add(no_hash)
+
+df = pd.DataFrame(data, columns=["id", "mention", "url"])
+df.to_csv("no_match.csv", index=False)
